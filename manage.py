c3166b9f553c53c2f02f752f64cc4c1dcd0b2e6a
VBRhuanzi <1139566880@qq.com>
2018-04-18 00:06:25 +0800

    分散封装配置，使用工场设计模式

--- manage.py
+++ manage.py
@@ -1,45 +1,21 @@
 # coding:utf-8
 # 程序入口 日期：2018.4.17 时间： 开发者： frank
-from flask import Flask
+
 from flask.ext.script import Manager
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from flask_migrate import Migrate,MigrateCommand
-from flask_wtf import CSRFProtect
-
-class Config(object):
-    """配置参数：重写系统默认的配置参数"""
-    DEBUG = True
-    # 配置mysql数据库 ， 真实开发不写127 ， 写数据库的真实ip
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/vbr_iHome"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    REDIS_HOST = 'localhost'
-    REDIS_PORT = 6379
+from flask_migrate import Migrate, MigrateCommand
 
 
-app = Flask(__name__)
 
-# 加载配置参数
-app.config.from_object(Config)
+# from iHome import app,db
+from iHome import get_app,db
 
-# 创建连接数据的对象,在配置后面
-db = SQLAlchemy(app)
-
-# 创建redis数据库链接对象 :
-# store:仓库
-# strict : 严格的；绝对的；精确的；详细的
-# StrictRedis类里面指定了host和port的缺省默认参数，可以不指定参数
-redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
-
-#开启csrf保护
-CSRFProtect(app)
+app = get_app('dev')
 
 # 创建脚本管理器
 manager = Manager(app)
 
 # 让迁移时，app和db建立链接,写注意不要搞反了
-Migrate(app,db)
+Migrate(app, db)
 
 # 将数据库迁移的脚本、命令添加到脚本管理器对象
 manager.add_command('db', MigrateCommand)
@@ -47,9 +23,11 @@ manager.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
-    redis_store.set("key","value")
-    return "index"
+    # redis_store.set("key", "value")
+    #
+    # session["name"] = 'ketty'
 
+    return "index"
 
 
 if __name__ == '__main__':
