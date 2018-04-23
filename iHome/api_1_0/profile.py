9a88201207bdab558beabde74baa7d47f1a7f0c5
VBRhuanzi <1139566880@qq.com>
2018-04-23 09:55:08 +0800

    上传用户头像接口后端逻辑实现

--- iHome/api_1_0/profile.py
+++ iHome/api_1_0/profile.py
@@ -1,16 +1,61 @@
 # coding:utf-8
 # 个人中心
-
+from iHome import db,constants
 from iHome.api_1_0 import api
-from flask import session, current_app, jsonify
+from flask import session, current_app, jsonify,request,
 from iHome.models import User
 from iHome.utils.response_code import RET
+from iHome.utils.image_storage import upload_image
 
-@api.route("/api/v1.0/user/avatar",methods=["POST"])
-def set_user_avatar():
-    """ """
-    pass
 
+
+@api.route("/user/avatar",methods=["POST"])
+def upload_avatar():
+    """
+    上传用户头像
+    0.TODO 判断用户是否登录
+    1.获取用户上传的头像数据，并校验
+    2.查询当前的登录用户
+    3.调用上传工具方法实现用户头像的上传
+    4.将用户头像赋值给当前的登录用户的user模型
+    5.将数据保存到数据库
+    6.响应上传用户头像的结果
+    """
+    # 1. 获取用户上传的头像数据，并校验
+    try:
+        avatar_data = request.files.get("avatar")
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg='获取用户头像失败')
+
+    # 2.查询当前的登录用户
+    user_id = session["user_id"]
+    try:
+        user = User.query.get(user_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='查询用户数据失败')
+    if not user:
+        return jsonify(errno=RET.NODATA, errmsg="用户不存在")
+
+    # 3.调用上传工具方法实现用户头像的上传
+    try:
+        key = upload_image(avatar_data)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg='上传用户头像失败')
+
+    # 4.将用户头像赋值给当前的登录用户的user模型
+    user.avatar_url = key
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='保存用户头像失败')
+
+    # 6.响应上传用户头像的结果
+    avatar_url = constants.QINIU_DOMIN_PREFIX + key
+    return jsonify(errno=RET.OK, errmsg="OK", data=avatar_url)
 
 
 
